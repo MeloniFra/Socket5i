9736a28b37a0410dbcbd4208abd888b6d36e0d03
Francesco <91307954+MeloniFra@users.noreply.github.com>
2023-05-05 22:29:02 +0200

    Second commit
    Implementato il Broadcast , verifica del sender, memorizzazione e classe Utente, da implementare l'aggiunta di utenti da interfaccia e la gestione di chat con utenti non in rubrica

--- Socket 5I/Socket 4I/MainWindow.xaml.cs
+++ Socket 5I/Socket 4I/MainWindow.xaml.cs
@@ -26,42 +26,27 @@ namespace Socket_4I
     {
 
         Socket socket;
-        List<string[]> utenti;
+        List<Utente> utenti;
         IPAddress IPLocale; 
         int portaLocale;
         IPAddress IPRemoto;
         int portaRemota;
-        Task task;
 
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();    
 
-            utenti = new List<string[]>();
-            utenti.Add(new string[] { "Aldo", "127.0.0.1", "10000" });
-            utenti.Add(new string[] { "Baldo", "127.0.0.1", "11000" });
-            utenti.Add(new string[] { "Carlo", "127.0.0.1", "12000" });
-            utenti.Add(new string[] { "Dean", "127.0.0.2", "10000" });
-            utenti.Add(new string[] { "Ettore", "127.0.0.3", "10000" });
-            
-            foreach(string[] s in utenti)
+            utenti = new List<Utente>();
+            utenti.Add(new Utente("Aldo", IPAddress.Parse("127.0.0.1"), 10000));
+            utenti.Add(new Utente("Baldo", IPAddress.Parse("127.0.0.1"), 11000));
+            utenti.Add(new Utente("Carlo", IPAddress.Parse("127.0.0.1"), 12000));
+            utenti.Add(new Utente("Dean", IPAddress.Parse("127.0.0.2"), 10000));
+            utenti.Add(new Utente("Ettore", IPAddress.Parse("127.0.0.3"), 10000));
+
+            foreach (Utente s in utenti)
             {
-                lstRubrica.Items.Add($"{s[0]} {s[1]} {s[2]}");
+                lstRubrica.Items.Add(s);
             }
-
-            Task.Run(AspettaMessaggio);
-
-            /*IPAddress local_address = IPAddress.Any;
-            IPEndPoint local_endpoint = new IPEndPoint(local_address, 10000);
-
-            socket = null;
-            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            //associo alla socket l'endpoint appena creato 
-            socket.Bind(local_endpoint);
-
-            //socket.Blocking = false;
-            //socket.EnableBroadcast = true;*/
-
         }
 
         private Task AspettaMessaggio()
@@ -73,31 +58,54 @@ namespace Socket_4I
             {
                 //ricezione dei caratteri in attesa
                 nBytes = socket.ReceiveFrom(buffer, ref remoteEndPoint);
+                
+                //prendo ip e porta del mittente
+                IPAddress IPMittente = ((IPEndPoint)remoteEndPoint).Address;
+                int portaMittente = ((IPEndPoint)remoteEndPoint).Port;
 
-                string from = ((IPEndPoint)remoteEndPoint).Address.ToString();
-
+                //decodifico il messaggio
                 string messaggio = Encoding.UTF8.GetString(buffer, 0, nBytes);
 
-                lstMessaggi.Dispatcher.Invoke(() => { lstMessaggi.Items.Add(lblContatto.Content + ": " + messaggio); });
+                //guardo se il mittente è già tra i contatti
+                bool presente = false;
+                foreach (Utente u in utenti)
+                {
+                    if (u.IP.Equals(IPMittente) && u.Porta == portaMittente)
+                    {
+                        presente = true;
+                        u.AggiungiMessaggio(u.Nome + ": " + messaggio);
+                        Utente mittente = new Utente();
+                        lstRubrica.Dispatcher.Invoke(() => { mittente = lstRubrica.SelectedItem as Utente; });
+                        if (mittente.IP.Equals(IPMittente) && mittente.Porta == portaMittente)
+                            lstMessaggi.Dispatcher.Invoke(() => { lstMessaggi.Items.Add(lblContatto.Content + ": " + messaggio); });
+                        else
+                        {
+                            MessageBox.Show(u.Nome + " ti ha mandato un messaggio.");
+                        }
+                        break;
+                    }
+                }
+
+                /*if (!presente)
+                {
+                    MessageBoxResult result = MessageBox.Show($"{IPMittente}:{portaMittente} sta cercando di inviarti un messaggio.\n" +
+                        $"vuoi salvarlo tra i contatti?", "Attenzione!", MessageBoxButton.OKCancel);
+                    if (result == MessageBoxResult.OK)
+                    {
+                        //implementa tu sai cosa
+                    }
+                }*/               
             }
         }
 
         private void btnInvia_Click(object sender, RoutedEventArgs e)
         {//metodo per inviare un messaggio
             try
-            {
-                if (string.IsNullOrEmpty(txtMessaggio.Text)) throw new Exception("Non puoi inviare un messaggio vuoto!");
+            {               
                 if (lstRubrica.SelectedIndex == -1) throw new Exception("Seleziona un contatto!");
                 CheckIpEPortaValidi();
 
-                //inizializzazione endpoint destinatario
-                IPEndPoint remote_endpoint = new IPEndPoint(IPRemoto, portaRemota);
-                
-                //traduzione da testo UTF8 a bytes per essere inviato
-                byte[] messaggio = Encoding.UTF8.GetBytes(txtMessaggio.Text);
-
-                //invio il messaggio al destinatario
-                socket.SendTo(messaggio, remote_endpoint);
+                InviaMessaggio(lstRubrica.SelectedItem as Utente ,txtMessaggio.Text);
 
                 //aggiungo il messaggio alla chat locale
                 lstMessaggi.Items.Add("Io: " + txtMessaggio.Text);
@@ -109,41 +117,56 @@ namespace Socket_4I
             }        
         }
 
+        private void InviaMessaggio(Utente destinatario, string msg)
+        {
+            if (string.IsNullOrEmpty(msg)) throw new Exception("Non puoi inviare un messaggio vuoto!");           
+
+            //inizializzazione endpoint destinatario
+            IPEndPoint remote_endpoint = new IPEndPoint(destinatario.IP, destinatario.Porta);
+
+            //traduzione da testo UTF8 a bytes per essere inviato
+            byte[] messaggio = Encoding.UTF8.GetBytes(msg);
+
+            //invio il messaggio al destinatario
+            socket.SendTo(messaggio, remote_endpoint);
+
+            destinatario.AggiungiMessaggio("Io: " + msg);
+        }
+
         private void btnConfermaIpPorta_Click(object sender, RoutedEventArgs e)
         {//metodo che inizializza la socket con IP e porta riportati nell'interfaccia
             try
             {
-                if (txtIP.IsEnabled)
+                MessageBoxResult result = MessageBox.Show("Sei sicuro di voler inserire questo IP e questa Porta?", "Attenzione!", MessageBoxButton.OKCancel);
+                if (result == MessageBoxResult.OK)
                 {
                     //prendo l'ip                   
                     bool IPok = IPAddress.TryParse(txtIP.Text, out IPLocale);
                     if (!IPok) throw new Exception("Ip non valido.");
                     //prendo la porta                   
                     bool portaOk = int.TryParse(txtPorta.Text, out portaLocale);
-                    if (!portaOk) throw new Exception("Porta non valida.");                   
+                    if (!portaOk) throw new Exception("Porta non valida.");
 
                     //inizializzo l'endpoint
                     IPEndPoint local_endpoint = new IPEndPoint(IPLocale, portaLocale);
-                                       
+
                     //inizializzo la socket
                     socket = null;
                     socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
                     //associo alla socket l'endpoint appena creato 
                     socket.Bind(local_endpoint);
 
-                    //disattivo i TextBox
+                    //disattivo i TextBox e il bottone
                     txtIP.IsEnabled = false;
                     txtPorta.IsEnabled = false;
+                    btnConfermaIpPorta.IsEnabled = false;
 
                     MessageBox.Show("Socket inizializzata correttamente.");
 
                     if (lstRubrica.SelectedIndex != -1) InizializzaIpEPortaClientRemoto();
-                }
-                else
-                {
-                    txtIP.IsEnabled = true;
-                    txtPorta.IsEnabled = true;
-                }
+
+                    Task.Run(AspettaMessaggio);
+                }           
             }
             catch(Exception ex)
             {
@@ -152,15 +175,22 @@ namespace Socket_4I
         }
 
         private void lstRubrica_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {//metodo che inizializza IP e Porta del client remoto
+        {//metodo che inizializza IP e Porta del client remoto e riempe la chat 
             try
             {
                 if (IPLocale != null && lstRubrica.SelectedIndex != -1)
                 {
                     CheckIpEPortaValidi();
 
+                    //pulisco la chat e poi la riempo con i messaggi vecchi
+                    lstMessaggi.Items.Clear();
+                    foreach(string s in utenti[lstRubrica.SelectedIndex].Chat)
+                    {
+                        lstMessaggi.Items.Add(s);
+                    }
+
                     InizializzaIpEPortaClientRemoto();                 
-                }                        
+                }        
             }
             catch (Exception ex)
             {
@@ -170,9 +200,9 @@ namespace Socket_4I
 
         private void CheckIpEPortaValidi()
         {//metodo che controlla se non si sta cercando di comunicare con se stessi      
-            string[] utente = utenti[lstRubrica.SelectedIndex];
+            Utente utente = utenti[lstRubrica.SelectedIndex];
 
-            if (IPLocale.ToString() == utente[1] && portaLocale.ToString() == utente[2])
+            if (IPLocale.Equals(utente.IP) && portaLocale == utente.Porta)
             {
                 lstRubrica.SelectedIndex = -1;
                 throw new Exception("Non puoi comunicare con te stesso!\n" +
@@ -182,10 +212,30 @@ namespace Socket_4I
 
         private void InizializzaIpEPortaClientRemoto()
         {//metodo che inizializza ip e porta del client remoto  
-            string[] utente = utenti[lstRubrica.SelectedIndex];
-            lblContatto.Content = utente[0];
-            IPRemoto = IPAddress.Parse(utente[1]);
-            portaRemota = int.Parse(utente[2]);
+            Utente utente = utenti[lstRubrica.SelectedIndex];
+            lblContatto.Content = utente.Nome;
+            IPRemoto = utente.IP;
+            portaRemota = utente.Porta;
+        }
+
+        private void btnBroadcast_Click(object sender, RoutedEventArgs e)
+        {          
+            try
+            {
+                foreach (Utente u in utenti)
+                {
+                    if (!IPLocale.Equals(u.IP) || portaLocale != u.Porta)
+                    {
+                        InviaMessaggio(u, txtMessaggio.Text);
+                    }
+                }
+
+                MessageBox.Show("Messaggio mandato in Broadcast.");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
---
+++ Socket 5I/Socket 4I/Utente.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Socket_4I
+{
+    internal class Utente
+    {
+        private string _nome;
+        private IPAddress _IP;
+        private int _porta;
+        private List<string> _chat;
+
+        public Utente(string nome, IPAddress IP, int porta)
+        {
+            _nome = nome;
+            _IP = IP;
+            _porta = porta;
+            _chat = new List<string>();
+        }
+
+        public Utente()
+        {
+
+        }
+
+        public string Nome
+        {
+            set 
+            {
+                if (!string.IsNullOrWhiteSpace(value)) _nome = value;
+                else throw new Exception("Inserisci un nome valido!");
+            }
+            get { return _nome; }
+        }
+
+        public IPAddress IP 
+        { 
+            set { _IP = value; }
+            get { return _IP; } 
+        }
+
+        public int Porta
+        {
+            set { _porta = value; }
+            get { return _porta; }
+        }
+
+        public List<string> Chat
+        {
+            get { return _chat; } 
+        }
+
+        public void AggiungiMessaggio(string messaggio) 
+        {
+            _chat.Add(messaggio);
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome} {IP}:{Porta}";
+        }
+    }
+}
